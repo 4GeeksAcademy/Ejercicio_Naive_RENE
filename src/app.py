3c10f88dad39ae791963970dad0a66a55384db29
ReneRamirez-Test <130073985+ReneRamirez-Test@users.noreply.github.com>
2025-04-22 01:41:15 +0000

    Naive_Ejercicio Final_Rene R

--- src/app.py
+++ src/app.py
@@ -1,4 +1,62 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from xgboost import XGBClassifier
+#import joblib
+
+df = pd.read_csv("DB_ clasificador de reseñas de la tienda de Google Play.csv")
+df.drop(columns=['package_name'], inplace=True)
+df['review'] = df['review'].str.strip().str.lower()
+
+X = df["review"]
+y = df["polarity"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+vec_model = CountVectorizer(stop_words="english")
+X_train_vec = vec_model.fit_transform(X_train).toarray()
+X_test_vec = vec_model.transform(X_test).toarray()
+
+nb_model = MultinomialNB()
+nb_model.fit(X_train_vec, y_train)
+y_pred = nb_model.predict(X_test_vec)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+bnb_model = BernoulliNB()
+bnb_model.fit(X_train_vec, y_train)
+y_pred_bnb = bnb_model.predict(X_test_vec)
+print("BernoulliNB - Accuracy:", accuracy_score(y_test, y_pred_bnb))
+print(classification_report(y_test, y_pred_bnb))
+
+gnb_model = GaussianNB()
+gnb_model.fit(X_train_vec, y_train)
+y_pred_gnb = gnb_model.predict(X_test_vec)
+print("GaussianNB - Accuracy:", accuracy_score(y_test, y_pred_gnb))
+print(classification_report(y_test, y_pred_gnb))
+
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model.fit(X_train_vec, y_train)
+y_pred_rf = rf_model.predict(X_test_vec)
+print("Random Forest - Accuracy:", accuracy_score(y_test, y_pred_rf))
+print(classification_report(y_test, y_pred_rf))
+
+lr_model = LogisticRegression(max_iter=1000)
+lr_model.fit(X_train_vec, y_train)
+y_pred_lr = lr_model.predict(X_test_vec)
+print("Logistic Regression - Accuracy:", accuracy_score(y_test, y_pred_lr))
+print(classification_report(y_test, y_pred_lr))
+
+xgb_model = XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=42)
+xgb_model.fit(X_train_vec, y_train)
+y_pred_xgb = xgb_model.predict(X_test_vec)
+print("XGBoost - Accuracy:", accuracy_score(y_test, y_pred_xgb))
+print(classification_report(y_test, y_pred_xgb))
+
